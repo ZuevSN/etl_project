75115a3f4af103bb764f21b6f61774284f31cc71
ZuevSN <ZuevSN86@gmail.com>
2026-08-23 17:42:06 +0300

    FIX

---
+++ etl_project/db_loader.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+
+# Строка подключения: dialect+driver://username:password@host:port/database
+DATABASE_URL = "postgresql+psycopg2://dev:devpassword@localhost:5432/etl_db"
+engine = create_engine(DATABASE_URL)
+
+def test_connection():
+    try:
+        with engine.connect() as connection:
+            print("Успешное подключение к PostgreSQL!")
+            # Простой тестовый запрос
+            result = connection.execute("SELECT version();")
+            print(result.fetchone()[0])
+    except Exception as e:
+        print(f"Ошибка подключения: {e}")
+
+if __name__ == "__main__":
+    test_connection()
